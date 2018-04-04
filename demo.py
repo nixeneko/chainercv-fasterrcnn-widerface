a8d281bf580e1df6f62c2658e15d49d3d1914e99
nixeneko <nixeneko@users.noreply.github.com>
2018-04-04 20:13:30 +0900

    add workaround for the newer version (maybe v0.7.0 and higer) of chainercv

--- demo.py
+++ demo.py
@@ -12,6 +12,10 @@ import download_model
 TRAINED_MODEL_DEFAULT = 'trained_model/snapshot_model.npz'
 MODEL_URL = 'http://nixeneko.2-d.jp/hatenablog/20170724_facedetection_model/snapshot_model.npz'
 
+# A pretrained model for the higher version of chainercv.
+TRAINED_MODEL_NEW = 'trained_model/snapshot_model_20180404.npz'
+MODEL_URL_NEW = 'http://nixeneko.2-d.jp/hatenablog/20170724_facedetection_model/snapshot_model_20180404.npz'
+
 def main():
     chainer.config.train = False
 
@@ -24,10 +28,21 @@ def main():
     if args.pretrained_model == TRAINED_MODEL_DEFAULT and \
        not os.path.exists(TRAINED_MODEL_DEFAULT):
         download_model.download_model(MODEL_URL, TRAINED_MODEL_DEFAULT)
-        
-    model = FasterRCNNVGG16(
-        n_fg_class=1,
-        pretrained_model=args.pretrained_model)
+    try:
+        model = FasterRCNNVGG16(
+            n_fg_class=1,
+            pretrained_model=args.pretrained_model)
+    
+    # Workaround for the newer version of chainercv (maybe v0.7.0 and higher)
+    except KeyError:
+        if args.pretrained_model == TRAINED_MODEL_DEFAULT and \
+           not os.path.exists(TRAINED_MODEL_NEW):
+            download_model.download_model(MODEL_URL_NEW, TRAINED_MODEL_NEW)
+            model = FasterRCNNVGG16(
+                n_fg_class=1,
+                pretrained_model=TRAINED_MODEL_NEW)
+        else: 
+            raise
 
     if args.gpu >= 0:
         model.to_gpu(args.gpu)
